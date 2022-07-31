6069d8c32388184acb6931fba4a8b29fb1a8dba0
rohit45k <mailxworks@gmail.com>
2022-07-31 17:15:50 +0530

    added chart in featured section

--- src/components/featured/chart/Chart.jsx
+++ src/components/featured/chart/Chart.jsx
@@ -1,7 +1,71 @@
 import classes from './Chart.module.scss';
+import {
+  AreaChart,
+  Area,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  ResponsiveContainer,
+} from 'recharts';
+
+const data = [
+  {
+    name: 'Jan',
+    total: 750,
+  },
+  {
+    name: 'Feb',
+    total: 1200,
+  },
+  {
+    name: 'Mar',
+    total: 800,
+  },
+  {
+    name: 'Apr',
+    total: 1100,
+  },
+  {
+    name: 'May',
+    total: 900,
+  },
+  {
+    name: 'Jun',
+    total: 1100,
+  },
+];
 
 const Chart = () => {
-  return <div className={classes.chart}>Chart</div>;
+  return (
+    <div className={classes.chart}>
+      <h3>Last 6 months revenue</h3>
+      <ResponsiveContainer width='100%'>
+        <AreaChart
+          data={data}
+          margin={{ top: 10, right: 30, left: 0, bottom: 0 }}
+        >
+          <defs>
+            <linearGradient id='total' x1='0' y1='0' x2='0' y2='1'>
+              <stop offset='5%' stopColor='#82ca9d' stopOpacity={0.8} />
+              <stop offset='95%' stopColor='#82ca9d' stopOpacity={0.3} />
+            </linearGradient>
+          </defs>
+          <XAxis dataKey='name' />
+          <YAxis />
+          <CartesianGrid strokeDasharray='3 3' className={classes.chartGrid} />
+          <Tooltip />
+          <Area
+            type='monotone'
+            dataKey='total'
+            stroke='#82ca9d'
+            fillOpacity={1}
+            fill='url(#total)'
+          />
+        </AreaChart>
+      </ResponsiveContainer>
+    </div>
+  );
 };
 
 export default Chart;
