c119b1ee11da7cbcabb430c6f9a5edcaf10bdf8f
rohit45k <mailxworks@gmail.com>
2022-07-31 15:25:32 +0530

    added widgets with dummy data

---
+++ src/components/widgets/Widgets.jsx
@@ -0,0 +1,46 @@
+import classes from './Widgets.module.scss';
+import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+
+import Card from '../UI/Card';
+
+const Widgets = ({ widget }) => {
+  const style = {
+    color: widget.iconColor,
+    backgroundColor: widget.iconBgColor,
+  };
+
+  return (
+    <Card>
+      <div className={classes.widget}>
+        <div className={classes.left}>
+          <span className={classes.title}>{widget.title}</span>
+          <span className={classes.counter}>
+            {widget?.money && '$'}
+            {widget.counter}
+          </span>
+          <span className={classes.link}>See details</span>
+        </div>
+        <div className={classes.right}>
+          <span
+            className={`${classes.stat} ${
+              widget.growth ? classes.positive : classes.negative
+            }`}
+          >
+            {widget.growth ? (
+              <KeyboardArrowUpIcon />
+            ) : (
+              <KeyboardArrowDownIcon />
+            )}
+            20%
+          </span>
+          <span className={classes.btn} style={style}>
+            {widget.icon}
+          </span>
+        </div>
+      </div>
+    </Card>
+  );
+};
+
+export default Widgets;
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,9 +1,61 @@
 import classes from './Home.module.scss';
 
+import Widgets from '../../components/widgets/Widgets';
+
+import PersonIcon from '@mui/icons-material/Person';
+import AccountBalanceWalletIcon from '@mui/icons-material/AccountBalanceWallet';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import MonetizationOnIcon from '@mui/icons-material/MonetizationOn';
+
+const DUMMY_WIDGET_DATA = [
+  {
+    id: 1,
+    title: 'USERS',
+    counter: 12223,
+    growth: true,
+    icon: <PersonIcon />,
+    iconColor: '#DB4437',
+    iconBgColor: '#ec9e98',
+  },
+  {
+    id: 2,
+    title: 'ORDERS',
+    counter: 1002,
+    growth: false,
+    icon: <ShoppingCartIcon />,
+    iconColor: '#4285F4',
+    iconBgColor: '#93b9f8',
+  },
+  {
+    id: 3,
+    title: 'EARNING',
+    counter: 12223,
+    growth: false,
+    icon: <MonetizationOnIcon />,
+    iconColor: '#0F9D58',
+    iconBgColor: '#8bf4c1',
+    money: true,
+  },
+  {
+    id: 4,
+    title: 'BALANCE',
+    counter: 1000,
+    growth: true,
+    icon: <AccountBalanceWalletIcon />,
+    iconColor: '#F4B400',
+    iconBgColor: '#ffdd7e',
+    money: true,
+  },
+];
+
 const Home = () => {
+  const widgets = DUMMY_WIDGET_DATA.map((widget) => (
+    <Widgets key={widget.id} widget={widget} />
+  ));
+
   return (
     <div className={classes.home}>
-      <h1 className={classes.title}>Hello again</h1>
+      <section className={classes.widgets}>{widgets}</section>
     </div>
   );
 };
