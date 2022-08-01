61c02bbb47a3ebca0fae7254ae6e45430b736c60
rohit45k <mailxworks@gmail.com>
2022-08-01 23:41:00 +0530

    users added

--- src/pages/users/Users.jsx
+++ src/pages/users/Users.jsx
@@ -1,7 +1,35 @@
 import classes from './Users.module.scss';
+import { DataGrid } from '@mui/x-data-grid';
+import { userRows, userColumns } from '../../data/users';
 
 const Users = () => {
-  return <div className={classes.users}>Users</div>;
+  const actions = [
+    {
+      field: 'actions',
+      headerName: 'Actions',
+      width: 200,
+      renderCell: () => {
+        return (
+          <div className={classes.btnGroup}>
+            <button className={classes.view}>View</button>
+            <button className={classes.delete}>Delete</button>
+          </div>
+        );
+      },
+    },
+  ];
+
+  return (
+    <div className={classes.users}>
+      <DataGrid
+        rows={userRows}
+        columns={userColumns.concat(actions)}
+        pageSize={10}
+        // rowsPerPageOptions={[10]}
+        checkboxSelection
+      />
+    </div>
+  );
 };
 
 export default Users;
